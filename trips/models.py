5db2d5744b4d50193ad9623e47d02e30f1db233b
fourlen <oborin.2013@mail.ru>
2022-07-03 02:11:34 +0300

    migrations

--- trips/models.py
+++ trips/models.py
@@ -7,11 +7,18 @@ class Trips(models.Model):
     price = models.IntegerField(blank=True, null=True)
     departure = models.CharField(max_length=100, blank=True, null=True)
     destination = models.CharField(max_length=100, blank=True, null=True)
+    start = models.IntegerField(blank=True, null=True)
+    end = models.IntegerField(blank=True, null=True)
 
 
     class Meta:
         managed = True
         db_table = 'trips'
+
+
+class TripsBooking(models.Model):
+    user = models.ForeignKey('users.Users', on_delete=models.CASCADE, blank=True, null=True)
+    trip = models.ForeignKey('trips.Trips', on_delete=models.CASCADE, blank=True, null=True)
 
 
     # owner = models.ForeignKey('users.Users', on_delete=models.CASCADE, blank=True, null=True)
--- trips/views.py
+++ trips/views.py
@@ -1,2 +1,3 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
