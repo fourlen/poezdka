5fb7f3f4898bc2a620cefb65d4db3ccd186c1946
fourlen <oborin.2013@mail.ru>
2022-08-08 01:05:19 +0500

    fixed #3

--- chat/routing.py
+++ chat/routing.py
@@ -1,11 +1,7 @@
 from django.urls import re_path
-from django.urls import path
-import views
-from . import consumers
+from chat.consumers import ChatConsumer
 
-cc = consumers.ChatConsumer()
 
 websocket_urlpatterns = [
-    re_path(r'ws/', cc.as_asgi()),
-    path('get_chat', views.get_chat)
+    re_path(r'ws/', ChatConsumer.as_asgi()),
 ]
