4ad267d5f155f82032fda32a75cd7a984dd1a23f
fourlen <oborin.2013@mail.ru>
2022-08-08 00:16:33 +0500

    added urls for chat

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
+from django.urls import path
+import views
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'/ws/chat/', consumers.ChatConsumer)
+    re_path(r'/ws/chat/', consumers.ChatConsumer),
+    path('get_chat', views.get_chat)
 ]
